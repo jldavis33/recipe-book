c8245b15f7033a21485ee0de5cef2bca41dd7e88
joshdavis <jldavis33@yahoo.com>
2022-12-22 21:35:47 -0600

    Create new ingredient and output to the ingredients array in the ShoppingListComponent

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.scss']
 })
 export class ShoppingEditComponent {
+    @ViewChild('ingredientName') ingredientName?: ElementRef;
+    @ViewChild('ingredientAmount') ingredientAmount?: ElementRef;
 
+    @Output() ingredientAdded = new EventEmitter<Ingredient>;
+
+    addShoppingListItem() {
+        let name = this.ingredientName?.nativeElement.value;
+        let amount = this.ingredientAmount?.nativeElement.value;
+
+        if (name && amount) {
+            this.ingredientAdded.emit(new Ingredient(
+                name,
+                parseInt(amount)
+            ));
+        }
+
+        console.log(name);
+        console.log(amount);
+    }
 }
