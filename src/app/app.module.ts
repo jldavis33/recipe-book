67a16aeb07c293abac89ae1d5a8f512b5d2f3dd7
joshdavis <jldavis33@yahoo.com>
2023-01-08 12:21:19 -0600

    Convert shopping-edit to Template-driven form - removes local/ViewChild input references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RouterLink } from '@angular/router';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -33,6 +34,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     imports: [
         BrowserModule,
         RouterLink,
+        FormsModule,
         AppRoutingModule
     ],
     providers: [ShoppingListService],
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,27 +1,29 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Ingredient } from '../../shared/models/ingredients.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.scss']
+    selector: 'app-shopping-edit',
+    templateUrl: './shopping-edit.component.html',
+    styleUrls: ['./shopping-edit.component.scss']
 })
 export class ShoppingEditComponent {
-    @ViewChild('ingredientName') ingredientName?: ElementRef;
-    @ViewChild('ingredientAmount') ingredientAmount?: ElementRef;
-
     constructor(private shoppingListService: ShoppingListService) { }
 
-    addShoppingListItem() {
-        let name = this.ingredientName?.nativeElement.value;
-        let amount = this.ingredientAmount?.nativeElement.value;
-
+    addShoppingListItem(name: string, amount: number) {
         if (name && amount) {
             this.shoppingListService.addIngredient(new Ingredient(
                 name,
-                parseInt(amount)
+                amount
             ));
         }
+    }
+
+    onAddItem(form: NgForm) {
+        this.addShoppingListItem(
+            form.value.name,
+            form.value.amount
+        );
     }
 }
