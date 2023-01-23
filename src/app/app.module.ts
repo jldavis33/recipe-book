2d297384e0ce4352c3832a747faba461c717ea4b
joshdavis <jldavis33@yahoo.com>
2023-01-23 17:09:22 -0600

    Provide RecipeService globally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { RouterLink } from '@angular/router';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
     declarations: [
@@ -38,7 +39,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
         ReactiveFormsModule,
         AppRoutingModule
     ],
-    providers: [ShoppingListService],
+    providers: [ShoppingListService, RecipeService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RecipeService } from './recipe.service';
 
 @Component({
     selector: 'app-recipes',
     templateUrl: './recipes.component.html',
-    styleUrls: ['./recipes.component.scss'],
-    providers: [RecipeService]
+    styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent { }
